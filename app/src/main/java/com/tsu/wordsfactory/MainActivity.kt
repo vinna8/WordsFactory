7c2d0cfac6299eeb56c66b2b96722fb2d3c09c39
vinna8 <vdovina.innusja@mail.ru>
2023-03-07 11:43:28 +0700

    logic indicators, button next, start and skip

--- app/src/main/java/com/tsu/wordsfactory/MainActivity.kt
+++ app/src/main/java/com/tsu/wordsfactory/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.tsu.wordsfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
+    private lateinit var indicatorsContainer: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setOnboardingItems()
+        setupIndicators()
+        setCurrentIndicator(0)
     }
 
     private fun setOnboardingItems() {
@@ -40,6 +48,7 @@ class MainActivity : AppCompatActivity() {
             ViewPager2.OnPageChangeCallback() {
                 override fun onPageSelected(position: Int) {
                     super.onPageSelected(position)
+                    setCurrentIndicator(position)
                 }
             })
         (onboardingViewPager.getChildAt(0) as RecyclerView).overScrollMode =
@@ -48,9 +57,64 @@ class MainActivity : AppCompatActivity() {
             if (onboardingViewPager.currentItem + 1 < onboardingItemsAdapter.itemCount) {
                 onboardingViewPager.currentItem += 1
             } else {
+                navigateToHomeActivity()
             }
         }
         findViewById<TextView>(R.id.textSkip).setOnClickListener{
+            navigateToHomeActivity()
+        }
+    }
+
+    private fun navigateToHomeActivity() {
+        startActivity(Intent(applicationContext, HomeActivity::class.java))
+        finish()
+    }
+
+    private fun setupIndicators() {
+        indicatorsContainer = findViewById(R.id.linearLayoutPagination)
+        val indicators = arrayOfNulls<ImageView>(onboardingItemsAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(12,0,12,0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let {
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+                it.layoutParams = layoutParams
+                indicatorsContainer.addView(it)
+            }
+        }
+    }
+
+    private fun setCurrentIndicator(position: Int) {
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainer.getChildAt(i) as ImageView
+            if (i == position) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+                if (position == childCount - 1) {
+                    findViewById<Button>(R.id.buttonOndoarding).setText(R.string.start)
+                } else {
+                    findViewById<Button>(R.id.buttonOndoarding).setText(R.string.next)
+                }
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+            }
         }
     }
 }
